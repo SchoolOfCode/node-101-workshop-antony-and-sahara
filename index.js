e308f1edcdf63a1813a10ce9ec663edffa97dfce
ajp64 <antonyjparnell@gmail.com>
2021-12-06 16:38:42 +0000

    Step 3 a complete

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-let myCollection = [
-    {
-      name: "School of Code mug",
-      count: 1,
-      whatILike: "It has my cute pixel character on it!"
-    },
-    {
-      name: "School of Code hat",
-      count: 2,
-      whatILike: "An often overlooked fashion accessory"
-    },
-    {
-      name: "School of Code pillow",
-      count: 1,
-      whatILike: "Eat. Sleep. Code. Repeat :)"
-    }
-  ];
 
 
   console.log(myCollection);
@@ -31,16 +14,14 @@ function describeItem(item){        // describe item taking in an object
 describeItem(myCollection[0]);  //calling the function with the first object in "myCollection"
 
 
-function describeCollection(somearray) {
-    for (let i=0; i<somearray.length; i++){
-        describeItem(somearray[i]);
+function describeCollection(somearray) {       // function takes an array
+    for (let i=0; i<somearray.length; i++){     // i goes through the length of the array
+        describeItem(somearray[i]);            // each time i increases, it looks at the next thing in the array (e.g. somearray[0]. somearray[1])
     }
 }
 
-describeCollection(myCollection);
+describeCollection(myCollection);                // calling function on my Collection
 
-// 👉 2d. Now make a function called `describeCollection` that takes in an array as a parameter. The function should loop through the array, and for each item, it should call the `describeItem` function so it displays a message according to how many you have in your collection.
 
-// Call your function below where you've defined it, handing in the `myCollection` array.
 
-// Run the file again to see the output and check that it outputs the correct message for each item in your collection (i.e. each object in your `myCollection` array).
+// moved myCollection to collection.js
