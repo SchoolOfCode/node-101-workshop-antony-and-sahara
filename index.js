80a1d1649bd82054f0dc8850d5fb012f0e3fb110
ajp64 <antonyjparnell@gmail.com>
2021-12-06 16:22:11 +0000

    2c completed

--- index.js
+++ index.js
@@ -18,3 +18,15 @@ let myCollection = [
 
 
   console.log(myCollection);
+
+//2c. Create a function called `describeItem`, which should take in an item as a parameter 
+function describeItem(item){        // describe item taking in an object
+  if (item.count === 1){
+  console.log(`I have a ${item.name}. Here's what I like about it: ${item.whatILike}`)    // if the item count property === 1, console log this
+  }else{
+  console.log(`I have ${item.count} ${item.name}'s. Here's what I like about them: ${item.whatILike}`)  // otherwise, console log this
+  }
+}
+
+describeItem(myCollection[0]);  //calling the function with the first object in "myCollection"
+
