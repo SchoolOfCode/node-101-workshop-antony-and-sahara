ba1ab5302d02bb07f7aca0ccb822f5b849499791
Sahara Rai <sahararai@hotmail.com>
2021-12-06 16:34:40 +0000

    task 2d done

--- index.js
+++ index.js
@@ -30,3 +30,17 @@ function describeItem(item){        // describe item taking in an object
 
 describeItem(myCollection[0]);  //calling the function with the first object in "myCollection"
 
+
+function describeCollection(somearray) {
+    for (let i=0; i<somearray.length; i++){
+        describeItem(somearray[i]);
+    }
+}
+
+describeCollection(myCollection);
+
+// 👉 2d. Now make a function called `describeCollection` that takes in an array as a parameter. The function should loop through the array, and for each item, it should call the `describeItem` function so it displays a message according to how many you have in your collection.
+
+// Call your function below where you've defined it, handing in the `myCollection` array.
+
+// Run the file again to see the output and check that it outputs the correct message for each item in your collection (i.e. each object in your `myCollection` array).
